473ac263f86ce4eebce77bd291ec186bfc0e0374
renelang <rene-lang@gmx.net>
2016-12-23 16:24:25 +0100

    Testezug die zweite

--- Leap/src/Main.java
+++ Leap/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Transformer transformer = new Transformer();
-		Dempster dempster = new Dempster();
+		//nur zu testzwecken
+		//Dempster dempster = new Dempster();
 		PointList pointList = transformer.readCSV();
 		System.out.println(pointList.getGesture());
 
